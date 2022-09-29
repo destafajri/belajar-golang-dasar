31bf4121a489ed4fe1403c66cdca229b3e8e5d9d
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-09-30 05:18:01 +0700

    belajar golang

---
+++ 16-Latihan(function sum dan calculate untuk mengitung nilai dari array)/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+)
+
+func main() {
+	//penjumlahan array(soal-1)
+	scores :=[]int {10, 5, 8, 9, 7}
+	hasil := sum(scores)
+	fmt.Println("hasil penjumlahan array", hasil)
+
+	//operasi matematika(soal-2)
+	//tanpa error
+	fmt.Println(calculation(5,5,"+"))
+	//dengan error
+	fmt.Println(calculation(5,6,"="))
+}
+
+//fungsi penjumlahan array
+func sum(nilai[]int)int{
+	var jumlah int
+	for i := 0; i < len(nilai); i++ {
+		jumlah += nilai[i]
+	}
+	return jumlah
+}
+
+//fungsi aritmatika multiple return dengan error message
+func calculation(nilai1, nilai2 int, operator string) (string,int, error){
+	if operator == "+"{
+		return "result =",nilai1 + nilai2, nil
+	}else if operator == "-"{
+		return "result =",nilai1 - nilai2, nil
+	}else if operator == "*"{
+		return "result =",nilai1*nilai2, nil
+	}else if operator == "/" {
+		return "result =",nilai1/nilai2, nil
+	}else{
+		return "", 0 , errors.New("operasi salah")
+	}
+}
