105771981e9e80e1196c78fbbd12017947bb46a0
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-09-30 04:46:50 +0700

    belajar golang

--- 15-Predefined return value/main.go
+++ 15-Predefined return value/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	luas, keliling :=luasdankeliling(5,6)
+	fmt.Println(luas, keliling)
+}
+
+//function multiple return dengan predefined return
+func luasdankeliling(p,l int) ( luas int, keliling int){
+	if p !=0 && l != 0{
+		luas = p*l
+		keliling = 2*(p+l)
+		return
+	}else {
+		return 0, 0
+	}
 }
